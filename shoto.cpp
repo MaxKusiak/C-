70ab9c623480468cff7c64a1280a4c31673c2540
MaxKusiak <maxkusiak4@gmail.com>
2022-10-04 21:36:34 +0300

    First commit

---
+++ shoto.cpp
@@ -0,0 +1,9 @@
+#include <clocale>
+#include <iostream>
+#include <ostream>
+#include <string>
+using namespace std;
+int main(){
+    setlocale(0, "");
+    cout << "Первая ветка или не я не знаю." << endl;
+}
